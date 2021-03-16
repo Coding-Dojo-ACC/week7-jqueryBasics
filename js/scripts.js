fed11c468d3d8408d8754efcd20edbc3c16a3c97
dojo24 <mlongenberger@codingdojo.com>
2021-03-15 19:44:39 -0500

    added more functons

--- js/scripts.js
+++ js/scripts.js
@@ -16,15 +16,25 @@ $(document).ready(function() {
     $('h1').click(function() {
         $('h1').css('color', 'purple');
     })
+
+    // Fade in nav links
     $('#topics').click(function(){
         $('a').fadeIn();
     })
+    
+    // Face in and out an img
     $('.bee').hover(function(){
         $('.bee').fadeOut();
     })
     $('#fadeInImg').hover(function(){
         $('.bee').fadeIn();
     })
+
+    // Adding in text with out an event
+    $('#append').append('We are adding text using jQuery!')
+
+    // add more text to the h3 tag using an event
+    $('#append').prepend("What's up?")
 })
 /*
 1. declare a function
