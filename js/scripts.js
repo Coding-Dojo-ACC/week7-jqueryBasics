34514f5170e7bf38fe3fa5d60f250aebb60adc51
dojo24 <mlongenberger@codingdojo.com>
2021-03-15 19:33:18 -0500

    end of lecture

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,33 @@
 $(document).ready(function() {
     // using the button tag hide and show the p tags
-
-
+    $('#hide').click(function(){
+        $('p').hide();
+    })
+    $('#show').click(function(){
+        $('p').show();
+    })
 
     // using the octocat class create an alert that states the user clicked on an octocat
+    $('.octocat').click(function(){
+        alert('You hovered over an Octocat!')
+    })
 
+    // add a color class to the h1 tag
+    $('h1').click(function() {
+        $('h1').css('color', 'purple');
+    })
+    $('#topics').click(function(){
+        $('a').fadeIn();
+    })
+    $('.bee').hover(function(){
+        $('img').fadeOut();
+    })
+    $('#fadeInImg').hover(function(){
+        $('.bee').fadeIn();
+    })
 })
+/*
+1. declare a function
+2. give instructions - hide just p tags
+
+*/
